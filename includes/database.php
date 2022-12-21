319d0c1d5000bed20208b1e71c02e2eaf28e15e8
mutiulahi <tesleemolamilekan902@gmail.com>
2022-12-21 20:07:07 +0100

    hi

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,5 @@
 <?php
-
-echo "Hello World";
-// $dbconnect =  new mysqli('localhost', 'tescode', 'fuhsi@@__cbt', 'cbt_fuhsi_db');
-// if($dbconnect->connect_errno){
-//     die("connection Failed: ".$dbconnect->connect_errno);
-// }
+$dbconnect =  mysqli_connect('localhost', 'tescode', 'fuhsi@@__cbt', 'cbt_fuhsi_db');
+if($dbconnect->connect_errno){
+    die("connection Failed: ".$dbconnect->connect_errno);
+}
