390a8929fce479e001fdde83341b13905217585c
mutiulahi <tesleemolamilekan902@gmail.com>
2022-12-21 19:59:24 +0100

    chibuke id issues fixed

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,7 @@
 <?php
-$dbconnect =  new mysqli('localhost', 'tescode', 'fuhsi@@__cbt', 'cbt_fuhsi_db');
-if($dbconnect->connect_errno){
-    die("connection Failed: ".$dbconnect->connect_errno);
-}
+
+echo "Hello World";
+// $dbconnect =  new mysqli('localhost', 'tescode', 'fuhsi@@__cbt', 'cbt_fuhsi_db');
+// if($dbconnect->connect_errno){
+//     die("connection Failed: ".$dbconnect->connect_errno);
+// }
