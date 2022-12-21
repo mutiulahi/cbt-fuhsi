f4dd255983501637652459c694db160c9d412286
mutiulahi <tesleemolamilekan902@gmail.com>
2022-12-21 20:13:50 +0100

    hi

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,5 @@
 <?php
 $dbconnect =  mysqli_connect('3.236.4.250', 'tescode', 'Fuhsi__123', 'cbt_fuhsi_db');
-if($dbconnect->connect_errno){
-    die("connection Failed: ".$dbconnect->connect_errno);
-}
+// if($dbconnect->connect_errno){
+//     die("connection Failed: ".$dbconnect->connect_errno);
+// }
